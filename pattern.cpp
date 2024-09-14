b86a32e60517666cd2125d80a4ad05fb31221fd1
Deepak singh <130153943+sdeepak83@users.noreply.github.com>
2024-09-14 11:47:13 +0530

    pattern

--- pattern.cpp
+++ pattern.cpp
@@ -1,2 +1,22 @@
 #include <iostream>
 using namespace std;
+
+int main()
+{
+
+    int hor;
+    int ver;
+    cout << "enter horizontzly num : ";
+    cin >> hor;
+    cout << "enter vertically num : ";
+    cin >> ver;
+
+    for (int i = 1; i <= ver; i++)
+    {
+        for (int j = 1; j <= hor; j++)
+        {
+            cout << "*";
+        }
+        cout << endl;
+    }
+}
